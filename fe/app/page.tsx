34271241e157bf8d4862068297d639e6ab794b24
Artem Petrenko <artem.petrenko@ibm.com>
2025-11-29 13:36:56 +0100

    fe

---
+++ fe/app/page.tsx
@@ -0,0 +1,32 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
+
+export default function Home() {
+  return (
+      <section>
+          <h1 className="text-center">
+              The hub for every dev <br /> Event you can&apos;t miss
+          </h1>
+          <p className="text-center mt-5">HAchatons, Meetings, Conferences, All in ONE Place</p>
+          <ExploreBtn />
+          <div className="mt-20 space-y-7">
+              <h3>Featured Events</h3>
+
+              <ul className="events">
+                  {events.map((event) => (
+                      <li key={event.title}>
+                          <EventCard {...event} />
+                      </li>
+
+
+                  ))}
+
+              </ul>
+
+          </div>
+
+
+      </section>
+  );
+}
---
+++ fe/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from 'next/image';
+import Logo from '@/public/icons/logo.png'
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <Link href='/' className="logo" >
+                    <Image src={Logo} alt="logo" width={24} height={24} />
+                    <p>DevEvent</p>
+                </Link>
+                <ul>
+                    <Link href="/">Home</Link>
+                    <Link href="/">Events</Link>
+                    <Link href="/">Create</Link>
+                </ul>
+            </nav>
+        </header>);
+}
+
+export default Navbar;
